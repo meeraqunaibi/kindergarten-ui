f5cf300ca5d81e4a1d966a34c39fc4d685fbdbae
raghadalmanasra <191055@ppu.edu.ps>
2023-04-23 21:41:12 +0200

    student reg componant

--- src/pages/signup/signup.page.jsx
+++ src/pages/signup/signup.page.jsx
@@ -1,9 +1,53 @@
-import React from "react";
 // import "./login.css";
+import React,{ useState, useEffect } from 'react'
 import loginLogo from "../../assets/images/loginLogo.svg";
 import graphics from "../../assets/images/signupPic.svg";
+import { Link, useNavigate } from "react-router-dom"
+import axios from 'axios'
 
 const Signup = () => {
+  const navigate = useNavigate();
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [validationErrors, setValidationErrors] = useState({});
+
+  useEffect(()=>{
+    if(localStorage.getItem('token') !== "" && localStorage.getItem('token') !== null){
+        navigate("/home-ppage");
+    }
+},[])
+
+const handelLogin= (e)=>{
+  e.preventDefault();
+  navigate('/login');
+}
+
+const registerAction = (e) => {
+  e.preventDefault();
+  setName(e.target.name.value.trim())
+  setEmail(e.target.email.value.trim())
+  setPassword(e.target.password.value.trim())
+  setIsSubmitting(true)
+  let regload = {
+      name:name,
+      email:email,
+      password:password,
+  }
+  axios.post('api/register', regload)
+  .then((r) => {
+      setIsSubmitting(false)
+      localStorage.setItem('token', r.data.token)
+      navigate("/home-ppage");
+  })
+  .catch((e) => {
+      setIsSubmitting(false)
+      if (e.response.data.errors !== undefined) {
+          setValidationErrors(e.response.data.errors);
+      }
+  });
+}
   return (
     <div className="container">
       <div className="graphics">
@@ -19,16 +63,16 @@ const Signup = () => {
       </div>
       <div className="login-info">
         <p>تسجيل حساب</p>
-        <form>
+        <form onSubmit={(e)=>registerAction(e)}>
           <label>اسم المستخدم</label>
-          <input type="name" placeholder="أدخل اسم المستخدم " />
+          <input type="text" placeholder="أدخل اسم المستخدم " />
           <label>الايميل</label>
           <input type="email" placeholder="أدخل الايميل " />
           <label>كلمة المرور</label>
-          <input type="password" placeholder="كلمة المرور " />
+          <input type="password" placeholder="كلمة المرور "/>
         </form>
-        <button>تسجيل الحساب</button>
-        <span>لديك حساب بالفعل؟ تسجيل الدخول</span>
+        <button disabled={isSubmitting} type="submit" >تسجيل الحساب</button>
+        <span onClick={handelLogin}>لديك حساب بالفعل؟ تسجيل الدخول</span>
       </div>
     </div>
   );
